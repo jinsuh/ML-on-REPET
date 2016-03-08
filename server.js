a29f1060362d0b6493547146b64a57c54b6ce46a
GregChan <gregorywarrenchan@gmail.com>
2016-03-08 00:16:22 -0600

    added more content

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static('public'));
+app.use('/public', express.static('public'));
 
 app.get('/', function (req, res) {
       res.sendFile(path.join(__dirname+'/views/index.html'));
